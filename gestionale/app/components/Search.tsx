ccfe3c4c24e83e318885b10c6046e0a985994c2d
G#sh <62940515+gerhash@users.noreply.github.com>
2024-01-23 09:42:24 +0100

    Creating Search page USESTATE Error
    problem with useState component
    probabilmente perchè non è visto come client

---
+++ gestionale/app/components/Search.tsx
@@ -0,0 +1,152 @@
+import React,{useState} from "react";
+
+
+const VisualizeResult = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+interface Student {
+  _id: number;
+  info: {
+    nome: string;
+    secondo_nome: string;
+    cognome: string;
+    dob: string; //data di nascita
+    lob: string; //luogo di nascita
+    prob: string; //provincia di nascita
+    capb: number; // cap luogo nascita
+    state: string; //stato di nascita
+    cf: string; //codice fiscale
+    res: string; //indirizzo residenza
+    cap_res: number; //CAP RESIDENZA
+    dom: string; //indirizzo domicilio
+    cap_dom: number; // CAP DOMICILIO
+    prefix_cell: string;
+    cellulare: string;
+    email: string;
+  };
+  payments: {
+    totale: number;
+    inviati: number;
+    da_dare: number;
+  };
+  courses_id: number[];
+  docs: {
+    n_doc: string; //numero documento fornito (CID, PATENTE, PASSAPORTO)
+    l_doc: string; //luogo rilascio documento fornito (COMUNE, MOTORIZZAZIONE)
+    city_doc: string; //CITTA DI RILASCIO documento fornito
+    rilascio_doc: string; //data rilascio
+    scadenza_doc: string; //data scadenza
+    immagini: {
+      path: string; //cartella studente
+      fronte: string;
+      retro: string;
+      vari_doc: { path: string }[];
+    };
+  };
+}
+
+const students = [
+  {
+          _id:38199209,
+          info:{
+              nome:"Luca", 
+              secondo_nome:"Jr", 
+              cognome:"Valori",
+              dob:"25-05-2001", //data di nascita
+              lob:"Benevento", //luogo di nascita
+              prob:"BN", //provincia di nascita
+              capb:82100, // cap luogo nascita
+              state:"Italy", //stato di nascita
+              cf:"LVHS01MSHWU12", //codice fiscale
+              res:"Via Avellino 28", //indirizzo residenza
+              cap_res:82100, //CAP RESIDENZA
+              dom:"Via Avellino 28", //indirizzo domicilio
+              cap_dom:82100, // CAP DOMICILIO
+              prefix_cell:"+39",
+              cellulare:"3662731403",
+              email:"gerardo.dagostino12@gmail.com",
+
+          },
+          payments:{
+              totale:3500,
+              inviati:2000,
+              da_dare:0,
+          },
+          courses_id:[
+              77764555
+          ],
+          docs:{
+              n_doc:"CA1177899", //numero documento fornito (CID, PATENTE, PASSAPORTO)
+              l_doc:"COMUNE", //luogo rilascio documento fornito (COMUNE, MOTORIZZAZIONE)
+              city_doc:"Benevento", //CITTA DI RILASCIO documento fornito
+              rilascio_doc:"22-01-2023", //data rilascio
+              scadenza_doc:"25-05-2033", //data scadenza
+              immagini:{
+                  path:"", //cartella studente
+                  fronte:"",
+                  retro:"",
+                  vari_doc:[
+                      {path:""}
+                  ]
+              }
+
+      }
+  }
+] 
+
+
+export default function Search() {
+    // Stato per gestire la barra di ricerca
+    const [searchQuery, setSearchQuery] = useState<string>('');
+
+    // Stato per gestire i filtri
+  const [filters, setFilters] = useState<{ someFilter: string }>({
+    someFilter: '',
+    // Aggiungi altri filtri con i relativi tipi
+  });
+
+  // Funzione per gestire la barra di ricerca
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+  
+  // Funzione per gestire i filtri
+  const handleFilterChange = (filterName: string, value: string) => {
+    setFilters({
+      ...filters,
+      [filterName]: value,
+    });
+  };
+
+
+  // Logica di filtraggio degli studenti
+  const filteredStudents: Student[] = students.filter((student) => {
+    // Aggiungi qui la logica di filtraggio in base a searchQuery e filters
+    // Esempio: filtra per nome
+    return student.info.nome.toLowerCase().includes(searchQuery.toLowerCase());
+  });
+
+    return (
+       <div className="container">
+        <div className="row text-center">
+          <h1 className="fs-1">Search Tool</h1>
+        </div>
+        <div id="searchinput" className="row">
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={searchQuery}
+          onChange={handleSearch}
+        />
+
+        </div>
+        <div className="row">
+            <VisualizeResult />
+        </div>
+       </div>
+      )}
--- gestionale/app/search/page.tsx
+++ gestionale/app/search/page.tsx
@@ -1,12 +1,12 @@
 import Navbar from "../components/Navbar"
-
-
-export default function Search() {
+import Search from "../components/Search"
+ 
+export default function Search_Page() {
     return (
       <main className="container-fluid d-flex flex-row">
         <Navbar />
         <div className="col-md-10 p-4">
-            <h1>SEARCH</h1>
+            <Search />
         </div>
       </main>
     )
