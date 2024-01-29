2a705ffb0ae8724a0a6982c489022e06f7f1dd9a
G#sh <62940515+gerhash@users.noreply.github.com>
2024-01-29 09:19:41 +0100

    Add Panel
    Aggiunta scheda di add dove è possibile scegliere "cosa aggiungere"

---
+++ gestionale/app/add/[type]/page.tsx
@@ -0,0 +1,288 @@
+'use client'
+import Navbar from "../../../components/Navbar"
+import { useState } from "react";
+import { Courses } from "@/app/components/Search";
+
+
+const UserDetails = ({ user, type }: { user: any, type:string }) => {
+    
+    //fai chiamata DB per i corsi_id che sono nell'array utente tutto relativo all'id dell'utente
+    
+    return (
+        <>
+            <div className="container">
+                Indietro
+                <h1>Dettagli {type.charAt(0).toUpperCase() + type.slice(1)}</h1>
+                <div className="row mt-5">
+                    <div className="col-7"> 
+                        <p className="mb-0">{user._id}</p>
+                        <h2>{user.info.nome} {user.info.secondo_nome} {user.info.cognome}</h2>
+                        <h3>{user.info.cf}</h3>
+                    </div>
+                    <div className="col-5 text-right ml-auto">
+                        <p className="mb-1">{user.info.prefix_cell + " " + user.info.cellulare}</p>
+                        <p>{user.info.email}</p>
+                    </div>
+                </div>
+                <div className="row mt-0 mb-0">
+                    <div className="col-7">
+                            <label>
+                            Data di Nascita:
+                        </label>
+                        <h2>{user.info.dob}</h2>
+                        <label>
+                            Luogo di Nascita:
+                        </label>
+                        <h2 className="mt-0">{user.info.lob} ({user.info.prob}), {user.info.state}</h2>
+                        <label>
+                            {user.info.capb}
+                        </label>
+                    </div>
+                    {(type == "student" ) && <div className="col-5 bg-primary text-white p-3 border rounded-3 fs-3 ">
+                        <label className="fs-5">
+                            Pagamenti:
+                        </label>
+                        <p className="mb-0 ">Tot: {user.payments.totale}</p>
+                        <p className="mb-0">Ricevuti: {user.payments.totale}</p>
+                        <p className="mb-0">In Sospeso: {user.payments.totale}</p>
+                    </div>}
+                </div>
+                <div className="row mt-0">
+                    <div className="col-4">
+                        <label>
+                            Indirizzo Residenza:
+                        </label>
+                        <h3>{user.info.res}</h3>
+                        <p>{user.info.cap_res}</p>
+                    </div>
+                    <div className="col-4">
+                        <label>
+                            Indirizzo di Domicilio
+                        </label>
+                        <h3>{user.info.dom}</h3>
+                        <p>{user.info.cap_dom}</p>
+                    </div>
+                  
+                </div>
+                
+                
+                {(type == "student" ) && 
+                    <div className="row bg-primary mt-0">
+                            <div className="col-4 bg-secondary">
+                                <ul>
+                                    <li>
+                                        Corso1 | In corso
+                                    </li>
+                                    <li>
+                                        Corso2 | Completo
+                                    </li>
+                                    <li>
+                                        Corso1 | Annullato
+                                    </li>
+                                    <li>
+                                        Corso2 | In corso
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-4 bg-warning">
+                                <p>{user.docs.n_doc} {user.docs.l_doc} {user.docs.city_doc}</p>
+                                <p> Emissione: {user.docs.rilascio} Scadenza{user.docs.scadenza}</p>
+                                <div>
+                                    <ul> //IMPLEMENTA CON DEGLI SCARICABILI
+                                        <li>FRONTE</li>
+                                        <li>RETRO</li>
+                                        <li>SELFIE?</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-4 bg-secondary">
+                                <p>Vari documenti</p>
+                            </div>
+                    </div>}
+                
+                {(type == "worker" ) && 
+                    <div className="row bg-primary mt-0">
+                        <ul>
+                            <li>
+                                Permesso 1 | o Si o No // implementa permessi
+                            </li>
+                        </ul>
+                    </div>}
+            </div>
+
+        </>
+    )
+}
+
+const CourseDetails = ({ course, type }: { course: Courses, type:string}) => {
+    
+    return (
+        <>
+            <div className="container">
+                Indietro
+                <h1>Dettagli {type.charAt(0).toUpperCase() + type.slice(1)}</h1>
+                <div className="row mt-5">
+                    <div className="col-7"> 
+                        <p className="mb-0">{course._id}</p>
+                        <h2>{course.nome}</h2>
+                        <h3>{course.ente}</h3>
+                    </div>
+                    <div className="col-5 text-right ml-auto">
+                        <p className="mb-1"></p>
+                        <p></p>
+                    </div>
+                </div>
+                <div className="row mt-4">
+                    <div className="col-7">
+                            <label>
+                            Data di Nascita:
+                        </label>
+                        <h2></h2>
+                        <label>
+                            Luogo di Nascita:
+                        </label>
+                        <h2></h2>
+                        <label>
+                          
+                        </label>
+                    </div>
+                    <div className="col-5 bg-primary text-white p-3 border rounded-3 fs-3 ">
+                        <label className="fs-5">
+                            Pagamenti:
+                        </label>
+                        <p className="mb-0 mt-4">Tot: </p>
+                        <p className="mb-0">Ricevuti: </p>
+                        <p className="mb-0">In Sospeso: </p>
+                    </div>
+                </div>
+                <div className="row mt-4">
+                    
+                    <div className="col-4">
+                        <label>
+                            Indirizzo Residenza:
+                        </label>
+                        <h3></h3>
+                        <p></p>
+                    </div>
+                    <div className="col-4">
+                        <label>
+                            Indirizzo di Domicilio
+                        </label>
+                        <h3></h3>
+                        <p></p>
+                    </div>
+                  
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+
+
+
+
+
+const tabDetails = ({ params })=> {
+    let student =   {
+    _id: 38199209,
+    info: {
+      nome: "Luca",
+      secondo_nome: "Jr",
+      cognome: "Valori",
+      dob: "25-05-2001",
+      lob: "Benevento",
+      prob: "BN",
+      capb: 82100,
+      state: "Italy",
+      cf: "LVHS01MSHWU12",
+      res: "Via Avellino 28",
+      cap_res: 82100,
+      dom: "Via Avellino 28",
+      cap_dom: 82100,
+      prefix_cell: "+39",
+      cellulare: "3662731403",
+      email: "gerardo.dagostino12@gmail.com",
+    },
+    payments: {
+      totale: 3500,
+      inviati: 2000,
+      da_dare: 0,
+    },
+    courses_id: [77764555, 29388899],
+    docs: {
+      n_doc: "CA1177899",
+      l_doc: "COMUNE",
+      city_doc: "Benevento",
+      rilascio_doc: "22-01-2023",
+      scadenza_doc: "25-05-2033",
+      immagini: {
+        path: "",
+        fronte: "",
+        retro: "",
+        vari_doc: [{ path: "" }],
+      },
+    },
+  }
+    let corso = 
+    {
+        _id:77764555,
+        nome:"EIPASS",
+        ente:"ANSIDONNA",
+        payments:{
+            prezzo_acquisto:20,
+            prezzo_vendita:140,
+            entrate:0,
+            uscite:0,
+            profitto:0,
+        },
+        numero_utenti:0,
+        id_utenti:[
+            38199209
+        ],
+    }
+    let worker = {
+        _id: 198328983,
+        info: {
+            nome: "Mario",
+            secondo_nome: "",
+            cognome: "Merola",
+            dob: "10-10-1998", //data di nascita
+            lob: "Roma", //luogo di nascita
+            prob: "RM", //provincia di nascita
+            capb: "00139", // cap luogo nascita
+            state: "Italy", //stato di nascita
+            cf: "ACACASBUACACASBU",  //codice fiscale
+            res: "Via delle Vie", //indirizzo residenza
+            cap_res: "00139", //CAP RESIDENZA
+            dom: "Via delle Vie", //indirizzo domicilio
+            cap_dom: "00139", // CAP DOMICILIO
+            prefix_cell: "+39",
+            cellulare: "3333333333",
+            email: "mail@gmai.com",
+          },
+          permessi:{
+            p1:"string"
+          }
+        }
+
+
+
+    const [courseData, setCourseData] = useState(corso);
+    const [studentData, setStudentData] = useState(student);
+    const [workerData, setWorkerData] = useState(worker);
+    console.log(params.id) //lo userai per selezionare lo specifico utente o corso o ente prendendolo da quando cliccano visualizza/
+    return (
+        <main className="container-fluid d-flex flex-row">
+                <Navbar />
+                <div className="col-md-10 p-4">
+                    { (params.type == "student" ) && <UserDetails type={params.type}  user={studentData} />}
+                    { (params.type == "worker") && <UserDetails type={params.type}  user={workerData} />}
+                    { (params.type == "corso") && <CourseDetails type={params.type} course={courseData} />}
+                </div>
+        </main>
+    );
+  }; 
+
+export default tabDetails;
---
+++ gestionale/app/add/page.tsx
@@ -0,0 +1,101 @@
+'use client'
+import Navbar from "../components/Navbar";
+import { useState } from "react";
+
+const redirect = (query:string) => {
+    const destinationValue = `/add/${query}`;
+    window.location.href = destinationValue;
+}
+
+
+
+const Ente = () => {
+    let query = "ente";
+    const handleRedirect = () => {
+        redirect(query);
+    }
+    return (
+        <div onClick={handleRedirect} style={{ cursor: 'pointer' }} className='bg-warning p-5 fs-1 d-flex flex-column h-100 justify-content-center align-items-center rounded-4'> {/* INSERISCI UNA FUNZIONE CHE AL CLICK REINDIRIZZI NELLA PAGINA DI ADD*/}
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-building" viewBox="0 0 16 16">
+                <path d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5z"/>
+                <path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1zm11 0H3v14h3v-2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V15h3z"/>
+        </svg>
+      
+      </div>
+    );
+};
+
+
+const Worker = () => {
+    let query = "worker";
+    const handleRedirect = () => {
+        redirect(query);
+    }
+    return (
+        <div onClick={handleRedirect} style={{ cursor: 'pointer' }} className='bg-warning p-5 fs-1 d-flex flex-column h-100 justify-content-center align-items-center rounded-4'> {/* INSERISCI UNA FUNZIONE CHE AL CLICK REINDIRIZZI NELLA PAGINA DI ADD*/}
+           <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-workspace" viewBox="0 0 16 16">
+            <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+            <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.4 5.4 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2z"/>
+            </svg>
+      
+      </div>
+    );
+};
+
+const Corso = () => {
+    let query = "corso";
+    const handleRedirect = () => {
+        redirect(query);
+    }
+    return (
+        <div onClick={handleRedirect} style={{ cursor: 'pointer' }} className='bg-warning p-5 fs-1 d-flex flex-column h-100 justify-content-center align-items-center rounded-4'> {/* INSERISCI UNA FUNZIONE CHE AL CLICK REINDIRIZZI NELLA PAGINA DI ADD*/}
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+                    <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917z"/>
+                    <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466z"/>
+            </svg>
+      
+      </div>
+    );
+};
+
+const Studente = () => {
+    let query = "student";
+    const handleRedirect = () => {
+        redirect(query);
+    }
+    return (
+        <div onClick={handleRedirect} style={{ cursor: 'pointer' }} className='bg-warning p-5 fs-1 d-flex flex-column h-100 justify-content-center align-items-center rounded-4'> {/* INSERISCI UNA FUNZIONE CHE AL CLICK REINDIRIZZI NELLA PAGINA DI ADD*/}
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"/>
+                </svg>
+      
+      </div>
+    );
+};
+
+
+const tabDetails = ({ })=> {
+
+    return (
+        <main className="container-fluid d-flex flex-row">
+                <Navbar />
+                <div className="col-md-10 p-4">
+                    <div className="d-flex flex-column align-items-center mt-3 gap-5">
+                            <h1 className="fs-1">Aggiungi</h1>
+
+                            <div className=" d-flex gap-5">
+                                <Studente />
+                                <Worker />
+                            </div>
+                            <div className=" d-flex gap-5">
+                                <Ente />
+                                <Corso />
+                            </div>
+                    </div>
+                </div>
+        </main>
+    );
+  }; 
+
+export default tabDetails;
