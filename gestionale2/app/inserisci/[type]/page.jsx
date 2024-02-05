8b8f0f19fa32ac4631821ff5ada207e59ec6e280
G#sh <62940515+gerhash@users.noreply.github.com>
2024-02-05 20:14:11 +0100

    preparazione loginpage

---
+++ gestionale2/app/inserisci/[type]/page.jsx
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+
+
+const ManagerMenu = ({params})  => {
+    
+    
+    let type = params.type
+
+    const [formData, setFormData] = useState(null)
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        try {
+          // Effettua la chiamata POST utilizzando Axios
+          const response = await axios.post(`http://127.0.0.1:2000/add/${type}`, formData);
+      
+          // Gestisci la risposta qui, ad esempio loggando la risposta
+          console.log('Risposta API:', response.data);
+      
+          // Resetta il form o esegui altre azioni necessarie
+        } catch (error) {
+          // Gestisci gli errori qui, ad esempio loggando l'errore
+          console.error('Errore API:', error);
+        }
+      };
+      
+    
+      
+      const province = [
+        "","AG", "AL", "AN", "AO", "AR", "AP", "AT", "AV", "BA", "BT", "BL", "BN", "BG", "BI", "BO", "BZ", "BS", "BR", "CA", "CL",
+        "CB", "CI", "CE", "CT", "CZ", "CH", "CO", "CS", "CR", "KR", "CN", "EN", "FM", "FE", "FI", "FG", "FC", "FR", "GE", "GO",
+        "GR", "IM", "IS", "AQ", "SP", "LT", "LE", "LC", "LI", "LO", "LU", "MC", "MN", "MS", "MT", "VS", "ME", "MI", "MO", "MB",
+        "NA", "NO", "NU", "OG", "OT", "OR", "PD", "PA", "PR", "PV", "PG", "PU", "PE", "PC", "PI", "PT", "PN", "PZ", "PO", "RG",
+        "RA", "RE", "RI", "RN", "RM", "RO", "SA", "VS", "SS", "SV", "SI", "SR", "SO", "TA", "TE", "TR", "TO", "OG", "TP", "TN",
+        "TV", "TS", "UD", "VA", "VE", "VB", "VC", "VR", "VV", "VI", "VT"
+      ];
+      const paesiOrdinati = ["Italia"]
+
+    
+    
+    return (
+        <>
+        
+
+        
+
+
+
+
+
+       
+        </>
+    )
+}
+
+
