1f7b580d666c71f6ce90f7dbfba98e029655fe37
G#sh <62940515+gerhash@users.noreply.github.com>
2024-02-06 09:27:04 +0100

    singolo componente
    aggiornare scheda singola magari facendo una tabella ;D

--- gestionale/app/manager/[type]/page.tsx
+++ gestionale/app/manager/[type]/page.tsx
@@ -4,15 +4,20 @@ import  { useState, ChangeEvent, FormEvent } from "react";
 import axios from "axios";
 import Link from "next/link";
 
+const province = [
+  "","AG", "AL", "AN", "AO", "AR", "AP", "AT", "AV", "BA", "BT", "BL", "BN", "BG", "BI", "BO", "BZ", "BS", "BR", "CA", "CL",
+  "CB", "CI", "CE", "CT", "CZ", "CH", "CO", "CS", "CR", "KR", "CN", "EN", "FM", "FE", "FI", "FG", "FC", "FR", "GE", "GO",
+  "GR", "IM", "IS", "AQ", "SP", "LT", "LE", "LC", "LI", "LO", "LU", "MC", "MN", "MS", "MT", "VS", "ME", "MI", "MO", "MB",
+  "NA", "NO", "NU", "OG", "OT", "OR", "PD", "PA", "PR", "PV", "PG", "PU", "PE", "PC", "PI", "PT", "PN", "PZ", "PO", "RG",
+  "RA", "RE", "RI", "RN", "RM", "RO", "SA", "VS", "SS", "SV", "SI", "SR", "SO", "TA", "TE", "TR", "TO", "OG", "TP", "TN",
+  "TV", "TS", "UD", "VA", "VE", "VB", "VC", "VR", "VV", "VI", "VT"
+];
+const paesiOrdinati = ["Italia"]
 
 
-
-
-
-const ManagerMenu = ({params})  => {
+const StudentAdd = () => {
   
-  let type = params.type
-
+  let type = "students"
   const [formData, setFormData] = useState({
     info: {
       nome: '',
@@ -55,7 +60,7 @@ const ManagerMenu = ({params})  => {
         vari: [],
       },
     },
-  });// CREA 4 COMPONENTI STUDENT ENTE CORSO E WORKER e ripeti questi comandi poi magari lo aggiusti
+  });
   const handleInputChange = (
     e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
@@ -104,29 +109,10 @@ const ManagerMenu = ({params})  => {
       console.error('Errore API:', error);
     }
   };
-  
 
-  
-  const province = [
-    "","AG", "AL", "AN", "AO", "AR", "AP", "AT", "AV", "BA", "BT", "BL", "BN", "BG", "BI", "BO", "BZ", "BS", "BR", "CA", "CL",
-    "CB", "CI", "CE", "CT", "CZ", "CH", "CO", "CS", "CR", "KR", "CN", "EN", "FM", "FE", "FI", "FG", "FC", "FR", "GE", "GO",
-    "GR", "IM", "IS", "AQ", "SP", "LT", "LE", "LC", "LI", "LO", "LU", "MC", "MN", "MS", "MT", "VS", "ME", "MI", "MO", "MB",
-    "NA", "NO", "NU", "OG", "OT", "OR", "PD", "PA", "PR", "PV", "PG", "PU", "PE", "PC", "PI", "PT", "PN", "PZ", "PO", "RG",
-    "RA", "RE", "RI", "RN", "RM", "RO", "SA", "VS", "SS", "SV", "SI", "SR", "SO", "TA", "TE", "TR", "TO", "OG", "TP", "TN",
-    "TV", "TS", "UD", "VA", "VE", "VB", "VC", "VR", "VV", "VI", "VT"
-  ];
-  const paesiOrdinati = ["Italia"]
-  
-
-return (
-    <div className="container-fluid d-flex flex-row">
-      <Navbar />
-      <div className="col-md-10 p-4">
-        <Link  href="/manager"><p>Indietro</p></Link>
-            
-            
-            creai componenti separati e pace
-            <form  onSubmit={handleSubmit} className="d-flex flex-column gap-5 p-3">
+  return   (
+        <>
+        <form  onSubmit={handleSubmit} className="d-flex flex-column gap-5 p-3">
                 <label>Add Student</label>
                 <button type="submit">Invia Dati</button>
 
@@ -401,10 +387,7 @@ return (
 
 
             </form>
-            
-      </div>
-
-      <style jsx>{`
+            <style jsx>{`
         
         .form h3{
           font-size:15px
@@ -426,6 +409,28 @@ return (
           height: 30px;
         }
       `}</style>
+        </>
+  )
+}
+
+
+const ManagerMenu = ({params})  => {
+  
+  let type = params.type
+
+return (
+    <div className="container-fluid d-flex flex-row">
+      <Navbar />
+      <div className="col-md-10 p-4">
+        <Link  href="/manager"><p>Indietro</p></Link>
+            
+            
+            <StudentAdd />
+            
+            
+      </div>
+
+ 
     </div>
   );
 };
