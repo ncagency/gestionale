9c524fa3b75842f05a117bdc4cb71928ec1ad553
G#sh <62940515+gerhash@users.noreply.github.com>
2024-02-12 15:55:58 +0100

    Open
    si apre al click ma si porta tutt p nanz

--- gestionalev3/components/Debts.tsx
+++ gestionalev3/components/Debts.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import Rate from './Rate'; // Assicurati di importare correttamente il componente Rate
+'use client' 
+import React, { useState } from 'react';
+import Rate from './Rate'; // Assicurati di importare il componente Rate correttamente
 
 const Debts = ({ rates, userId }) => {
-  return (
-    <div className="container">
-      {rates.map((rateGroup, index) => (
-        <div key={index}>
-          <h2>Debito {index + 1}</h2>
-          <Rate data={rateGroup} id={userId} index_debito={index} />
+    const [openIndex, setOpenIndex] = useState(null);
+
+    // Funzione per gestire il click del div
+    const handleDivClick = (index) => {
+        setOpenIndex((prevOpenIndex) => (prevOpenIndex === index ? null : index));
+    };
+
+    return (
+        <div className="container">
+            {rates.map((rateGroup, index) => (
+                <div key={index} className='flex-column m-2 text-white' onClick={() => handleDivClick(index)}>
+                    <div className='bg-primary p-2 rounded-3 w-50'><h2 className='fs-5'>{index + 1}) Rate</h2></div>
+                    {openIndex === index && <Rate data={rateGroup} id={userId} index_debito={index} />}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default Debts;
