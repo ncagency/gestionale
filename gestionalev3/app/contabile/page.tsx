0773420e284fac0ff6dcfa2d405c850c91990783
G#sh <62940515+gerhash@users.noreply.github.com>
2024-02-13 11:54:55 +0100

    Visualizza Dati in tabelle

--- gestionalev3/app/contabile/page.tsx
+++ gestionalev3/app/contabile/page.tsx
@@ -5,21 +5,33 @@ import axios from 'axios';
 
 
 const Contabile = () => {
-    const [contabile,setContabile] = useState([])
+    const [students,setStudents] = useState([])
+    const [courses,setCourses] = useState([])
+    const [enti,setEnti] = useState([])
+    const [cronologia,setCronologia] = useState([])
+
+
+
 
     useEffect(() => {
         const fetchFileNames = async () => {
           try {
             const response = await axios.get(`http://127.0.0.1:2000/contabile`);
-            setContabile(response.data)
-          } catch (error) {
+            let contabile = response.data
+            
+            setStudents(contabile[0].students)
+            setCourses(contabile[0].courses)
+            setEnti(contabile[0].enti)
+            setCronologia(contabile[0].cronologia_transazioni)
+        
+        } catch (error) {
             console.error('Errore durante il recupero dei nomi dei file:', error);
           }
         };
     
         fetchFileNames();
       });
-      console.log(contabile[0])
+  console.log(enti)
 
   return (
     <div className='h-100 d-flex flex-column gap-3'>
@@ -48,27 +60,88 @@ const Contabile = () => {
             </div>
 
 
-            <div className='d-flex gap-2 w-100 h-75 bg-primary p-4'  >
+            <div className='d-flex gap-2 w-100 h-75 bg-primary p-4 rounded-4'  >
+                    
+                    <div className='w-50 d-flex flex-column '> 
+                    <h1 className='text-white fs-4'>Enti</h1>
+                    <table className="table table-bordered ">
+                            <tbody>
+                                <tr className="border-1 p-4">
+                                    <td>Nome</td>
+                                    <td>Totale</td>
+                                    <td>Da Inviare</td>
+                                    <td>Inviati</td>
+                                </tr>
 
-                    <div className='w-50 d-flex '> 
-                    <table className="table table-bordered">
-                        <tbody>
-
-                            <TableRow label="ID" value="we" />
-                          
-                        </tbody>
+                        {enti.map((ente) => (
+                                <tr className="border-1 p-4">
+                                    <td>{ente.nome}</td>
+                                    <td>{ente.totale}</td>
+                                    <td>{ente.da_inviare}</td>
+                                    <td>{ente.inviati}</td>
+                                </tr>                       
+                        ))}
+                        
+                    </tbody>
                     </table>
+
                     </div>
-                    <div className='w-50 d-flex '> 
-                    <table className="table table-bordered">
-                        <tbody>
+                    <div className='w-50 d-flex flex-column'> 
+                    <h1 className='text-white fs-4'>Corsi</h1>
+                        <table className="table table-bordered">
+                                <tbody>
+                                    <tr className="border-1 p-4">
+                                        <td>Nome</td>
+                                        <td>Costo</td>
+                                        <td>Venduti</td>
+                                        <td>Stock</td>
+                                        <td>Entrate</td>
+                                        <td>Uscite</td>
+                                        <td>Profit</td>
+                                    </tr>
+
+                            {courses.map((corso) => (
+                                    <tr className="border-1 p-4">
+                                        <td>{corso.name}</td>
+                                        <td>{corso.costo}</td>
+                                        <td>{corso.venduti}</td>
+                                        <td>{corso.stock}</td>
+                                        <td>{corso.totale_entrate}</td>
+                                        <td>{corso.totale_uscite}</td>
+                                        <td>{corso.totale_profit}</td>
+                                    </tr>                       
+                            ))}
                             
-                            <TableRow label="ID" value="we" />
-                          
                         </tbody>
-                    </table>
+                        </table>
                     </div>
-                   
+                                
+            </div>
+
+            <div className='w-100 bg-primary p-4 rounded-4'>
+            <h1 className='text-white'>Studenti</h1>
+            <table className="table table-bordered">
+                                <tbody>
+                                    <tr className="border-1 p-4">
+                                        <td>Nome</td>
+                                        <td>Totale</td>
+                                        <td>Saldati</td>
+                                        <td>In Sospeso</td>
+                                        <td>Iscrizioni</td>
+                                    </tr>
+
+                            {students.map((student) => (
+                                    <tr className="border-1 p-4">
+                                        <td>{student._id}</td>
+                                        <td>{student.totale}</td>
+                                        <td>{student.saldati}</td>
+                                        <td>{student.in_sospeso}</td>
+                                        <td>{student.rate.length}</td>
+                                    </tr>                       
+                            ))}
+                            
+                        </tbody>
+                        </table>
             </div>
     </div>
   )
