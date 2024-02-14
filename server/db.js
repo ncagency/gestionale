18a73f7137c385c397121fdf214965444aa406d4
G#sh <62940515+gerhash@users.noreply.github.com>
2024-02-14 15:41:32 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@ const { MongoClient } = require('mongodb');
 
 let dbConnection
 
-const uri = "mongodb+srv://gerrydag:Mulignan_1324@cluster0.g7jbh.mongodb.net/ansidonna";
+const uris = "mongodb+srv://gerrydag:Mulignan_1324@cluster0.g7jbh.mongodb.net/ansidonna";
 
 module.exports = {
     connectToDb: (cb) => {
-        MongoClient.connect(uri)
+        MongoClient.connect(uris)
             .then((client) => {
                 dbConnection = client.db()
                 return cb()
