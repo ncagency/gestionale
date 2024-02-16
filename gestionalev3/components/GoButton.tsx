c76f158e15ac39aaf68eec1205a7c877ca281383
G#sh <62940515+gerhash@users.noreply.github.com>
2024-02-16 19:28:52 +0100

    agiiustati

--- gestionalev3/components/GoButton.tsx
+++ gestionalev3/components/GoButton.tsx
@@ -24,7 +24,7 @@ const GoButton: React.FC<GoButtonProps> = ({ destination  }) => {
       className='px-4 py-1 fs-5 ms-auto align-self-end rounded-4 '
       style={{ cursor: 'pointer', background: 'linear-gradient(to right, #a44b8b, #ffb6f5) '}} // Aggiungi uno stile per mostrare che è cliccabile
     >
-      <i className="text-white">-></i>
+      <i className="text-white">-{'>'}</i>
     </div>
   );
 }
