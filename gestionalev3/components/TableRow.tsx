0013e7e3723bfc127ff1e84d5adef2a5246f84f5
G#sh <62940515+gerhash@users.noreply.github.com>
2024-02-18 10:58:42 +0100

    si & no
    non visualizzava true e false

--- gestionalev3/components/TableRow.tsx
+++ gestionalev3/components/TableRow.tsx
@@ -1,9 +1,16 @@
 const TableRow = ({ label, value }:{ label:any, value:any }) => {
-    console.log(value)
+    let text;
+
+    if (value == true) {
+        text = "Si"
+    } else {
+        text = "No"
+    }
+
     return ( 
         <tr className="border-1 p-4">
         <td>{label}</td>
-        <td>{value}</td>
+        <td>{text}</td>
     </tr>
     )
   
