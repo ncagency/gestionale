5eb75c7895fae613b803204dffd428eee7ee0aab
G#sh <62940515+gerhash@users.noreply.github.com>
2024-02-18 10:55:38 +0100

    test

--- gestionalev3/components/TableRow.tsx
+++ gestionalev3/components/TableRow.tsx
@@ -1,10 +1,13 @@
-const TableRow = ({ label, value }:{ label:any, value:any }) => (
-   
-    <tr className="border-1 p-4">
-         <td>{label}</td>
-         <td>{value}</td>
-     </tr>
- );
+const TableRow = ({ label, value }:{ label:any, value:any }) => {
+    console.log(value)
+    return ( 
+        <tr className="border-1 p-4">
+        <td>{label}</td>
+        <td>{value}</td>
+    </tr>
+    )
+  
+};
 
 
 export default TableRow
