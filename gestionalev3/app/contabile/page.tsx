d661f4e4ab407adc175b4bc683e16355d410200e
G#sh <62940515+gerhash@users.noreply.github.com>
2024-02-19 12:41:48 +0100

    LV LV

--- gestionalev3/app/contabile/page.tsx
+++ gestionalev3/app/contabile/page.tsx
@@ -124,7 +124,7 @@ function Contabile() {
             
             <div className='d-flex gap-1 h-25'>
                 <div style={styleSuccess} className='d-flex flex-column justify-content-center bg-primary w-25 h-100 rounded-3 p-3'>
-                   <div className='d-flex gap-2 align-items-center '>
+                   <div className='d-flex gap-2 justify-content-between align-items-center p-1 '>
                 
                     <p className='fs-4 text-white'>Entrate</p>
 
