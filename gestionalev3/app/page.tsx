c3f22c10d13a0b8bb8c9a5fdea5a78810e3e3790
G#sh <62940515+gerhash@users.noreply.github.com>
2024-02-19 12:48:11 +0100

    fr

--- gestionalev3/app/page.tsx
+++ gestionalev3/app/page.tsx
@@ -122,11 +122,15 @@ cronologia.forEach(item => {
       <div style={style} className='d-flex flex-column position-relative p-4 h-100 rounded-4'>
         <div className="row w-100 h-75 p-1 m-2">
             <div className="col-6">
-                <p className="fs-5 text-white">i</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" className="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                        <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                        </svg>
                 <p className="fs-1 text-white">{total} €</p>
             </div>   
             <div className="col-6">
-                <p className="fs-5 text-white">i</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" className="bi bi-caret-up-fill" viewBox="0 0 16 16">
+                            <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+                    </svg>
                 <p className="fs-1 text-white">{uscite} €</p>
             </div>   
         </div>
