700a5e6fc00b0bf9d70008d75d95b438990ac498
sbangete <62940515+gerhash@users.noreply.github.com>
2024-02-20 14:40:51 +0100

    sda

---
+++ gestionalev3/app/boh/page.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+const UploadFile = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0] || null;
+    setSelectedFile(file);
+  };
+
+  const handleUpload = async () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('actualFile', selectedFile);
+
+      try {
+        const response = await fetch('/api/insert', {
+          method: 'POST',
+          body: formData
+        });
+
+        if (response.ok) {
+          console.log('File uploaded successfully');
+        } else {
+          console.error('Failed to upload file:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error uploading file:', error);
+      }
+    } else {
+      console.error('No file selected');
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+};
+
+export default UploadFile;
