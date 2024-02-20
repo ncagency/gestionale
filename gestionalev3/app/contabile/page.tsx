8b6ecd1bab7f11f7ab103d7671fd455163b6a94b
G#sh <62940515+gerhash@users.noreply.github.com>
2024-02-20 11:40:52 +0100

    facm

--- gestionalev3/app/contabile/page.tsx
+++ gestionalev3/app/contabile/page.tsx
@@ -120,27 +120,53 @@ function Contabile() {
                 <p onClick={() => redirec(`/`)} style={{cursor: 'pointer'}}>Indietro</p>
             </div>
 
-          
+            <div className='d-flex gap-1 h-25'>
+
+                <div style={styleSuccess} className='d-flex flex-column justify-content-center bg-primary w-25 h-100 rounded-3 p-3'>
+                  
+                   <div className='d-flex gap-2 justify-content-between  p-1 '>
+                
+                    <p className='fs-4 text-white'>Entrate</p>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" className="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                        <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                        </svg>
+
+
+                    </div>
+                   <p className='fs-1 text-white'>{total} €</p>
+                </div>
+                <div style={styleBad} className='d-flex flex-column justify-content-center  bg-primary w-25 h-100 rounded-3 p-3'>
+                   
+                <div className='d-flex gap-2 justify-content-between  p-1 '>
+                
+                <p className='fs-4 text-white'>Uscite</p>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" className="bi bi-caret-up-fill" viewBox="0 0 16 16">
+                    <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+                    </svg>
+
+                </div>
+                   <p className='fs-1 text-white'>{uscite} €</p>
+                </div>
+                <div style={style} className='d-flex flex-column justify-content-center  bg-primary w-25 h-100 rounded-3 p-3'>
+                   
+                <div className='d-flex gap-2 justify-content-between  p-1 '>
+                
+                <p className='fs-4 text-white'>Profit</p>
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" className="bi bi-cash-coin" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8m5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0"/>
+                <path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195z"/>
+                <path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083q.088-.517.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1z"/>
+                <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 6 6 0 0 1 3.13-1.567"/>
+                </svg>
+                
+
+                </div>
+                   <p className='fs-1 text-white'>{profit} €</p>
+                </d
            
-
-          {monthYearFilter && (
-            <div key={monthYearFilter}>
-                <h2>{monthYearFilter}</h2>
-                <p>Entrate: {totalPerMonthYear[monthYearFilter].entrata} €</p>
-                <p>Uscite: {totalPerMonthYear[monthYearFilter].uscita} €</p>
-                <p>Profitto: {totalPerMonthYear[monthYearFilter].profitto} €</p>
-            </div>
-            )}
-        <div>
-        <select onChange={(e) => setMonthYearFilter(e.target.value)}>
-            {Object.keys(totalPerMonthYear).map((monthYear) => (
-                <option key={monthYear} value={monthYear}>
-                {monthYear}
-                </option>
-            ))}
-            </select>
-
-        </div>
 
              <div style={style} className='w-100 d-flex flex-column p-4 rounded-4'> 
                     <h1 className='text-white fs-4'>Enti</h1>
@@ -221,6 +247,7 @@ function Contabile() {
                         </tbody>
                         </table>
             </div>
+</div>
     </div>
   )
     
